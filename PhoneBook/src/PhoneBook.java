49590c69a2beeace13bfa19977e4cd35eca04917
ExampleAccountSkillFactory <skill.email.example@gmail.com>
2020-06-26 05:38:28 +0300

    Move project and add phone book structure.

---
+++ PhoneBook/src/PhoneBook.java
@@ -0,0 +1,30 @@
+public class PhoneBook {
+
+    public static void main(String[] args) {
+        //Добавить считывание ввода пользователя в цикле
+    }
+
+    public static boolean checkPhoneNumber(String phoneNumber) {
+        return true;
+    }
+
+    public static boolean checkName(String name) {
+        return true;
+    }
+
+    public static String formatName(String name) {
+        return "";
+    }
+
+    public static String formatPhoneNumber(String number) {
+        return "";
+    }
+
+    public static void add(String[][] book, String name, String number) {
+        //add logic
+    }
+
+    public static void list(String[][] book) {
+        //print phone book
+    }
+}
