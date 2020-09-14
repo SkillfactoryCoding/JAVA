9edf473f749baff0386912a90337cd2cd4834e41
Andrey <ankrnv@gmail.com>
2020-09-14 18:36:14 +0300

    Add more comments!

--- PhoneBook/src/PhoneBook.java
+++ PhoneBook/src/PhoneBook.java
@@ -81,14 +81,14 @@ public class PhoneBook {
         return ("+7" + " " + a.substring(1, 4) + " " + a.substring(4, 7) + " " + a.substring(7, 9) + " " + a.substring(9));
     }
 
-    public static String[][] add(String[][] book, String name, String number, int i) {    // сама телефонная книга, точнее ее заготовка
+    public static String[][] add(String[][] book, String name, String number, int i) {    // массив-заготовка телефонной книги
         book[i][0] = name;
         book[i][1] = number;
         list(book);
         return book;
     }
 
-    public static void list(String[][] book) {
+    public static void list(String[][] book) {              // список контактов
         for (int r = 0; r < book.length; r++) {
             if (book[r][0] != null) {
                 System.out.println(book[r][0] + ":" + " " + book[r][1]);
