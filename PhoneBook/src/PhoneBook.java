74248b2c41f2f46f3b0fb1bd99e1580fb1a0989e
Eugene <ev.savelov@gmail.com>
2020-09-14 18:31:24 +0300

    метод main

--- PhoneBook/src/PhoneBook.java
+++ PhoneBook/src/PhoneBook.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
+
 public class PhoneBook {
 
     public static void main(String[] args) {
-        //Добавить считывание ввода пользователя в цикле
-    }
+        int i = 0;
+        String[][] book = new String[20][2];
+        while (true) {
+            String name = "";
+            boolean mark = true;
+            String number = "";
+            System.out.println("Введите ФИО");
+            Scanner scanner = new Scanner(System.in);
+            boolean isCorrectName = false;
+            while (!isCorrectName) {
+                name = scanner.nextLine(); //Считывает строку из System.in
+                isCorrectName = checkName(name);
+                if (!isCorrectName) {
+                    System.out.println("Введите корректное ФИО!");
+                } else {
+                    name = formatName(name);
+                }
+            }
+
+
 
     public static boolean checkPhoneNumber(String phoneNumber) {
         return true;
