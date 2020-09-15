4473bab2ce94897824b1f9260d28e45f27e74774
Andrey <ankrnv@gmail.com>
2020-09-15 18:22:37 +0300

    First test

---
+++ Students_HW2/.mvn/src/Main.java
@@ -0,0 +1,63 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Task[] tasks = generateTasks();
+
+        Mentor firstMentor = new Mentor("Egor", 35);
+        Mentor secondMentor = new Mentor("Igor", 40);
+
+        Student[] students = new Student[4];
+
+        students[0] = new Student("Antonio",89, firstMentor);
+        students[1] = new Student("Andrey", 35, secondMentor);
+        students[2] = new Student("Eugene", 30, secondMentor);
+        students[3] = new Student("Viktor", 30, firstMentor);
+
+        while (!doesNotEachStudentSolveAllTasks(students)) {
+            /*for (int i = 0; i < students.length - 1; i++) {
+                Student students = students[i];
+            }*/
+            for (Student student : students) {
+                int tasksToSolve = randomInt(5, tasks.length - 1);
+                student.solveTasks(tasks, tasksToSolve);
+            }
+        }
+    }
+
+    private static boolean doesNotEachStudentSolveAllTasks(Student[] students) {
+        boolean done = true;
+        for(Student student : students){
+            boolean d = student.isAreAllTasksDone();
+            done &= d;
+        }
+        return done;
+
+    }
+
+    private static Task[] generateTasks() {
+        Task[] tasks = new Task[31];
+        for (int i = 0; i < 7; i++) {
+            Task t = new Test(i + 1, "Test Task " + (i + 1) + "text");
+            tasks[i] = t;
+        }
+        for (int i = 7; i < 18; i++) {
+            Task t = new DragAndDrop(i + 1, "DragAndDrop Task" + (i + 1) + "text");
+            tasks[i] = t;
+        }
+        for (int i = 18; i < 31; i++) {
+            Task t = new WriteCode(i + 1, "Write Code Task " + (i + 1) + " text");
+            tasks[i] = t;
+        }
+        return tasks;
+    }
+
+    public static int randomInt(int min, int max) {
+        Random random = new Random();
+        int diff = max - min;
+        int result = random.nextInt(diff + 1);
+        result += min;
+        return result;
+    }
+}
---
+++ Students_HW2/.mvn/src/Student.java
@@ -0,0 +1,64 @@
+public class Student extends Person {
+
+    public static int tasksSum;
+
+    public int solvedTasks;
+    public Mentor mentor;
+    public boolean allTasksSolved = false;
+
+    public Student(String name, int age, Mentor mentor) {
+        super(name, age);
+        this.mentor = mentor;
+        this.solvedTasks = 0;
+    }
+
+    public int getSolvedTasks() {
+        return solvedTasks;
+    }
+
+    public void setSolvedTasks(int solvedTasks) {
+        this.solvedTasks = solvedTasks;
+    }
+
+    public Mentor getMentor() {
+        return mentor;
+    }
+
+    public void setMentor(Mentor mentor) {
+        this.mentor = mentor;
+    }
+
+    public boolean isAreAllTasksDone() {
+        return allTasksSolved;
+    }
+
+    public void solveTasks(Task[] tasks, int tasksToSolve) {
+        // solvedTasks = 2
+        // tasksToSolve = 2
+        // [0] [1] [2] [3] [4] [5]
+        System.out.println("[" + getName() + "] already solved ["+solvedTasks+"] and ["+ tasksToSolve+"] new tasks to solve");
+        for (int i = solvedTasks; i < solvedTasks + tasksToSolve; i++) {
+
+            solveTask(tasks[i]);
+            if (solvedTasks >= tasks.length) {
+                allTasksSolved = true;
+                System.out.println("[" + getName() + "] solved all tasks!");
+                return;
+            } else {
+                allTasksSolved = false;
+            }
+        }
+
+    }
+
+    private void solveTask(Task task) {
+        if (task instanceof AutoChecked) {
+            ((AutoChecked) task).checkAutomatically();
+        } else {
+            while (!mentor.checkCode(task)) {
+            }
+        }
+        solvedTasks++;
+        tasksSum++;
+    }
+}
