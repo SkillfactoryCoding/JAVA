bf07ce60bdcfbbbf48498f5ade47a822a77abfb0
Zavalishchev <Andrey.Zavalishchev@gmail.com>
2020-11-08 14:39:04 +0300

    удалил всё лишнее

--- Camunda BPM Demo/src/src/Kitchen.java
+++
@@ -1,8 +0,0 @@
-public class Kitchen {
-    public void cookPasta() {
-        System.out.println("Вскипятить воду");
-        System.out.println("Положить в воду макароны");
-        System.out.println("Подождать");
-        System.out.println("Слить воду");
-    }
-}
